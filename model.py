bb5dbaf061b2657288d08ec1bb7320957d9181f9
iareadrian <adrian.araza289@gmail.com>
2023-07-12 03:40:22 -0700

    Fixed errors. Data models ready for seeding

--- model.py
+++ model.py
@@ -18,14 +18,14 @@ class Student(db.Model):
     gender = db.Column(db.String(20))
     height = db.Column(db.Integer)
     weight = db.Column(db.Integer)
-    # sport_name = db.Column(db.String(30), db.ForeignKey('sports.sport_name')) 
+    sport_name = db.Column(db.String(30), db.ForeignKey('sports.sport_name')) 
     position_id = db.Column(db.Integer, db.ForeignKey('positions.position_id'))
-    # FIXME location_id = db.Column(db.Integer, db.ForeignKey('locations'))
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
     bio = db.Column(db.String)
 
     position = db.relationship('Position', back_populates='students')
-    # sport = db.relationship('Sport', back_populates='students')
-    # FIXME location = db.relationship('Location', back_populates='students')
+    sport = db.relationship('Sport', back_populates='students')
+    location = db.relationship('Location', back_populates='students')
 
     def __repr__(self):
         return (
@@ -35,123 +35,121 @@ class Student(db.Model):
         )
     
 
-# class Coach(db.Model):
-#     '''Coach user'''
+class Coach(db.Model):
+    '''Coach user'''
 
-#     __tablename__ = 'coaches'
+    __tablename__ = 'coaches'
 
-#     coach_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     coach_email = db.Column(db.String(50), unique=True)
-#     coach_password = db.Column(db.String(50))
-#     fname = db.Column(db.String(30))
-#     lname = db.Column(db.String(30))
-#     FIXME school_id = db.Column(db.Integer, db.ForeignKey('schools'))
-#     FIXME sport_name = db.Column(db.String(30), db.ForeignKey('sports'))
-#     bio = db.Column(db.String)
+    coach_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    coach_email = db.Column(db.String(50), unique=True)
+    coach_password = db.Column(db.String(50))
+    fname = db.Column(db.String(30))
+    lname = db.Column(db.String(30))
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.school_id'))
+    sport_name = db.Column(db.String(30), db.ForeignKey('sports.sport_name'))
+    bio = db.Column(db.String)
 
-#     school = db.relationship('School', back_populates='coaches')
-#     sport = db.relationship('Sport', back_populates='coaches')
+    school = db.relationship('School', back_populates='coaches')
+    sport = db.relationship('Sport', back_populates='coaches')
 
-#     def __repr__(self):
-#         return (
-#             f'<Coach coach_id = {self.coach_id}, '
-#             f'coach_email = {self.coach_email}, '
-#             f'fname = {self.fname}>'
-#         )
+    def __repr__(self):
+        return (
+            f'<Coach coach_id = {self.coach_id}, '
+            f'coach_email = {self.coach_email}, '
+            f'fname = {self.fname}>'
+        )
 
 
-# class Sport(db.Model):
-#     '''Type of sport played or coached'''
+class Sport(db.Model):
+    '''Type of sport played or coached'''
 
-#     __tablename__ = 'sports'
+    __tablename__ = 'sports'
 
-#     sport_name = db.Column(db.String(30), primary_key=True)
+    sport_name = db.Column(db.String(30), primary_key=True)
     
-#     # students = db.relationship('Student', back_populates='sport')
-#     # positions = db.relationship('Position', back_populates='sport')
-#     # coaches = db.relationship('Coach', back_populates='sport')
-#     # schools = db.relationship('School', secondary='sports_schools', back_populates='sports')
+    students = db.relationship('Student', back_populates='sport')
+    positions = db.relationship('Position', back_populates='sport')
+    coaches = db.relationship('Coach', back_populates='sport')
+    schools = db.relationship('School', secondary='sports_schools', back_populates='sports')
 
-#     def __repr__(self):
-#         return (
-#             f'<Sport sport_name = {self.sport_name}, '
-#             f'sport_id = {self.sport_id}>'
-#         )
+    def __repr__(self):
+        return f'<Sport sport_name = {self.sport_name}>'
+            
 
+class Position(db.Model):
+    '''Athlete's position on the team'''
 
-# class Position(db.Model):
-#     '''Athlete's position on the team'''
+    __tablename__ = 'positions'
 
-#     __tablename__ = 'positions'
+    position_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    position_name = db.Column(db.String(30))
+    sport_name = db.Column(db.String(30), db.ForeignKey('sports.sport_name'))
 
-#     position_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     position_name = db.Column(db.String(30))
-#     FIXME sport_name = db.Column(db.String(30), db.ForeignKey('sports'))
-
-#     students = db.relationship('Student', back_populates='position')
+    students = db.relationship('Student', back_populates='position')
+    sport = db.relationship('Sport', back_populates='positions')
     
-#     def __repr__(self):
-#         return (
-#             f'<Position position_id = {self.position_id}, '
-#             f'position_name = {self.position_name}>'
-#         )
+    def __repr__(self):
+        return (
+            f'<Position position_id = {self.position_id}, '
+            f'position_name = {self.position_name}>'
+        )
 
 
-# class School(db.Model):
-#     '''A college'''
+class School(db.Model):
+    '''A college'''
 
-#     __tablename__ = 'schools'
+    __tablename__ = 'schools'
 
-#     school_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     school_name = db.Column(db.String)
-#     FIXME location_id = db.Column(db.Integer, db.ForeignKey('locations'))
+    school_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    school_name = db.Column(db.String)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
 
-#     location = db.relationship('Location', back_populates='schools')
-#     sports = db.relationship('Sport', secondary='sports_schools', back_populates='schools')
+    location = db.relationship('Location', back_populates='schools')
+    coaches = db.relationship('Coach', back_populates='school')
+    sports = db.relationship('Sport', secondary='sports_schools', back_populates='schools')
+     
 
-#     def __repr__(self):
-#         return (
-#             f'<School school_id = {self.school_id}, '
-#             f'school_name = {self.school_name}>'
-#         ) 
+    def __repr__(self):
+        return (
+            f'<School school_id = {self.school_id}, '
+            f'school_name = {self.school_name}>'
+        ) 
 
 
-# class SportSchool(db.Model):
-#     '''Links the Sport and School classes'''
+class SportSchool(db.Model):
+    '''Links the Sport and School classes'''
 
-#     __tablename__ = 'sports_schools'
+    __tablename__ = 'sports_schools'
 
-#     sportschool_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     sport_name = db.Column(db.String(30), db.ForeignKey('sports.sports_name'))
-#     school_id = db.Column(db.Integer, db.ForeignKey('schools.school_id'))
+    sportschool_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    sport_name = db.Column(db.String(30), db.ForeignKey('sports.sport_name'))
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.school_id'))
 
-#     # TODO: Create relationships
-
-#     def __repr__(self):
-#         return (
-#             f'<SportSchool sportschool_id = {self.sportschool_id}, '
-#             f'sport_name = {self.sport_name}>'
-#         ) 
+    def __repr__(self):
+        return (
+            f'<SportSchool sportschool_id = {self.sportschool_id}, '
+            f'sport_name = {self.sport_name}>'
+        ) 
     
 
-# class Location(db.Model):
-#     '''U.S. locations by city and state'''
+class Location(db.Model):
+    '''U.S. locations by city and state'''
 
-#     __tablename__ = 'locations'
+    __tablename__ = 'locations'
 
-#     location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     city = db.Column(db.String(30))
-#     state = db.Column(db.String(30))
+    location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    city = db.Column(db.String(30))
+    state = db.Column(db.String(30))
 
-#     schools = db.relationship('School', back_populates='location')
-#     students = db.relationship('Student', back_populates='location')
+    schools = db.relationship('School', back_populates='location')
+    students = db.relationship('Student', back_populates='location')
 
-#     def __repr__(self):
-#         return (
-#             f'<Location location_id = {self.location_id}, '
-#             f'city = {self.city}, '
-#             f'state = {self.state}>'
-#         )
+    def __repr__(self):
+        return (
+            f'<Location location_id = {self.location_id}, '
+            f'city = {self.city}, '
+            f'state = {self.state}>'
+        )
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///team_up', echo=True):
