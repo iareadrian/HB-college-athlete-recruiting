c1eca2858b25c723ccfd8b4379e025aa2c377ab3
iareadrian <adrian.araza289@gmail.com>
2023-07-14 04:16:35 -0700

    Seed file creates test student users

--- seed_db.py
+++ seed_db.py
@@ -11,12 +11,21 @@ os.system('createdb team_up')
 model.connect_to_db(server.app)
 model.db.create_all()
 
+# Creating only student users at this point
+
 basketball = crud.create_sport('basketball')
 model.db.session.add(basketball)
+model.db.session.commit()
 
-point_guard = crud.create_position('point guard', basketball)
+point_guard = crud.create_position('point guard', basketball.sport_name)
 model.db.session.add(point_guard)
+model.db.session.commit()
 
+city_state = crud.create_location('Oakland', 'California')
+model.db.session.add(city_state)
+model.db.session.commit()
+
+# Create 3 test student users
 for n in range(3):
     student_email = f'test_user{n}@test.com'
     student_password = 'test'
@@ -26,8 +35,13 @@ for n in range(3):
     height = 75
     weight = 190
     bio = 'Chef Curry with the shot'
-    sport_name = basketball
-    position = 1
-    location = 1
+    sport_name = basketball.sport_name
+    position = point_guard.position_id
+    location = city_state.location_id
 
-    # TODO finish creating test data
+    user = crud.create_student_user(student_email, student_password, fname,
+                                    lname, gender, height, weight, bio,
+                                    sport_name, position, location)
+
+    model.db.session.add(user)
+    model.db.session.commit()
