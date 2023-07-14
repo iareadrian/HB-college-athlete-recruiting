5ec756729e72f4489f0b6ed92540387955eb6ccb
iareadrian <adrian.araza289@gmail.com>
2023-07-14 16:39:46 -0700

    Created a secret key, route to home page, and a base.html template

--- server.py
+++ server.py
@@ -3,12 +3,19 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from jinja2 import StrictUndefined
 from model import connect_to_db, db
+import crud
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
 app.jinja_env.undefined = StrictUndefined
 
 
-# TODO: Insert routes here
+@app.route('/')
+def homepage():
+    '''View the home page'''
+
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__':
