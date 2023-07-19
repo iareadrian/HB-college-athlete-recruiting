f5d53aa5a12ef17c41dc44d23238bf6bf2f7e92d
iareadrian <adrian.araza289@gmail.com>
2023-07-18 17:03:33 -0700

    Created functions to seed db with coach users

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 '''CRUD operations'''
 
-from model import db, Student, Sport, Position, Location, connect_to_db
+import model
+
 
 def create_student_user(student_email,
                         student_password,
@@ -11,11 +12,11 @@ def create_student_user(student_email,
                         weight,
                         bio,
                         sport_name=None,
-                        position_id = None,
+                        position_id=None,
                         location_id=None):
     '''Create and return a new student user'''
 
-    student = Student(student_email=student_email,
+    student = model.Student(student_email=student_email,
                       student_password=student_password,
                       fname=fname,
                       lname=lname,
@@ -31,14 +32,14 @@ def create_student_user(student_email,
 def create_sport(sport_name):
     '''Create and return a new sport'''
 
-    sport = Sport(sport_name=sport_name)
+    sport = model.Sport(sport_name=sport_name)
 
     return sport
 
 def create_position(position_name, sport_name=None):
     '''Create and return a new positon'''
 
-    position = Position(position_name=position_name, sport_name=sport_name)
+    position = model.Position(position_name=position_name, sport_name=sport_name)
 
     return position
 
@@ -46,7 +47,7 @@ def create_position(position_name, sport_name=None):
 def create_location(city, state):
     '''Create and return a new city and state'''
 
-    location = Location(city=city, state=state)
+    location = model.Location(city=city, state=state)
 
     return location
 
@@ -54,18 +55,45 @@ def create_location(city, state):
 def get_student_by_email(student_email):
     '''Return a student user by email'''
 
-    return Student.query.filter(Student.student_email == student_email).first()
+    return model.Student.query.filter(model.Student.student_email == student_email).first()
 
 
 def create_student_login(student_email, student_password):
     '''Create and return new student login credentials'''
 
-    student_login = Student(student_email=student_email,
+    student_login = model.Student(student_email=student_email,
                             student_password=student_password)
 
     return student_login
 
 
+def create_coach_user(coach_email,
+                      coach_password,
+                      fname,
+                      lname,
+                      bio,
+                      school_id=None,
+                      sport_name=None):
+    '''Create and return a new coach user'''
+
+    coach = model.Coach(coach_email=coach_email,
+                        coach_password=coach_password,
+                        fname=fname,
+                        lname=lname,
+                        bio=bio,
+                        school_id=school_id,
+                        sport_name=sport_name)
+
+    return coach
+
+def create_school(school_name, location_id=None):
+    '''Create and return a new school'''
+
+    school = model.School(school_name=school_name, location_id=location_id)
+
+    return school
+
+
 if __name__ == '__main__':
     from server import app
-    connect_to_db(app)
+    model.connect_to_db(app)
--- seed_db.py
+++ seed_db.py
@@ -11,7 +11,7 @@ os.system('createdb team_up')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-# Creating only student users at this point
+# Creating test student and coach users
 
 basketball = crud.create_sport('basketball')
 model.db.session.add(basketball)
@@ -21,13 +21,13 @@ point_guard = crud.create_position('point guard', basketball.sport_name)
 model.db.session.add(point_guard)
 model.db.session.commit()
 
-city_state = crud.create_location('Oakland', 'California')
-model.db.session.add(city_state)
+student_city_state = crud.create_location('Oakland', 'California')
+model.db.session.add(student_city_state)
 model.db.session.commit()
 
 # Create 3 test student users
 for n in range(3):
-    student_email = f'test_user{n}@test.com'
+    student_email = f'student_user{n}@test.com'
     student_password = 'test'
     fname = 'Chef'
     lname = 'Curry'
@@ -37,11 +37,22 @@ for n in range(3):
     bio = 'Chef Curry with the shot'
     sport_name = basketball.sport_name
     position = point_guard.position_id
-    location = city_state.location_id
+    location = student_city_state.location_id
 
-    user = crud.create_student_user(student_email, student_password, fname,
+    student_user = crud.create_student_user(student_email, student_password, fname,
                                     lname, gender, height, weight, bio,
                                     sport_name, position, location)
 
-    model.db.session.add(user)
+    model.db.session.add(student_user)
     model.db.session.commit()
+
+
+# Create 3 test coach users
+for n in range(3):
+    coach_email = f'coach_user{n}@test.com'
+    coach_password = 'test'
+    fname = 'Mark'
+    lname = 'Fox'
+    bio = "Mark Fox is the men's basketball coach at Cal"
+    school_id =
+    school_sport = basketball.sport_name
